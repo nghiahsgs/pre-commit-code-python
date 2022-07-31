80d15a882da9ebf37684275836c57c250913b723
Sotatek-NghiaNguyen4 <nghia.nguyen4@sotatek.com>
2022-07-31 11:26:25 +0700

    zzz

---
+++ test_back.py
@@ -0,0 +1,12 @@
+# ======================
+# myAnotherPythonFile.py
+# ======================
+def calculate_something(a, b, c, d, e, f, g, h):
+    i = a + b
+    j = (c + d) / 2
+    k = e * f
+    m = g - h
+    # Check if i is an odd number
+    if (i % 2) == 1:
+        i += 1
+    return i + j + k + m
